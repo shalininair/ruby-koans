8bb8ce710d2b1094d0224b74dec1fee01b60256b
Shalini Nair <shalininair75@gmail.com>
2017-01-26 02:17:26 +0000

    Solve from the triangle project to blocks

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,12 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  # This also works 
+  #raise TriangleError if [a,b,c].min  <= 0  
+  x,y,z = [a,b,c].sort
+  raise TriangleError if x <= 0 
+  raise TriangleError if x + y <= z
+  
   if (a == b) and (a == c) 
       :equilateral
   elsif (( a!= b) and (a != c) and (b != c))
